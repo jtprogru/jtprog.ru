cde7a1b075627fbce4b4bf6d4af5b5e5f19a6c24
JTProgru <jtprogru@gmail.com>
2020-12-29 15:04:08 +0300

    Add notify to TG

---
+++ .cicd/notify-tg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+TIME="10"
+URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
+TEXT="Deploy status: $1%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
+
+curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
+
+echo "The notification is sent"
+
